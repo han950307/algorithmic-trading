cb42224417ca37412fbb0173e1201005971571e1
= <han950307@gmail.com>
2018-09-29 18:35:42 -0400

    converted to spaces for indentation instead of tabs

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -2,16 +2,16 @@ from abc import ABC
 
 
 class Algorithm(ABC):
-	"""
-	Base class for all stock trading algorithms.
-	The idea is when we instantiate a stock trading service,
-	we can pass in a trading algorithm as an argument and it would be
-	swappable.
-	"""
-	pass
+    """
+    Base class for all stock trading algorithms.
+    The idea is when we instantiate a stock trading service,
+    we can pass in a trading algorithm as an argument and it would be
+    swappable.
+    """
+    pass
 
 
 class SimpleAlgorithm(Algorithm):
-	"""
-	Very basic algorithm for trading stock.
-	"""
+    """
+    Very basic algorithm for trading stock.
+    """
